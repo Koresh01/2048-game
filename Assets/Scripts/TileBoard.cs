833581e2dc04afe06526ca8f40df1170e5e47991
Koresh01 <kmatveev2004@gmail.com>
2025-10-24 01:49:01 +0300

    Сделал экран Game Over

--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TileBoard : MonoBehaviour
 {
+    public GameManager gameManager;
+
     /// <summary>
     /// Префаб квадратика с цифрой.
     /// </summary>
@@ -33,13 +35,29 @@ public class TileBoard : MonoBehaviour
         tiles = new List<Tile>();
     }
 
-    private void Start()
-    {
-        CreateTile();
-        CreateTile();
-    }
 
-    private void CreateTile()
+    /// <summary>
+    /// Удаляет все плитки с игрового поля.
+    /// </summary>
+    public void ClearBoard()
+    {
+        foreach (var cell in grid.cells)
+        {
+            cell.tile = null;
+        }
+
+        foreach (var tile in tiles)
+        {
+            Destroy(tile.gameObject);
+        }
+
+        tiles.Clear();
+    }
+    
+    /// <summary>
+    /// Создаёт плитку в рандомном свободном слоту.
+    /// </summary>
+    public void CreateTile()
     {
         Tile tile = Instantiate(tilePrefab, grid.transform);
         tile.SetState(tileStates[0], 2);
@@ -203,7 +221,52 @@ public class TileBoard : MonoBehaviour
         {
             CreateTile();
         }
-        // TODO: check for game over
+        
+
+        // Проверка на GameOver:
+        if (CheckForGameOver())
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    private bool CheckForGameOver()
+    {
+        if (tiles.Count != grid.size)
+        {
+            return false;
+        }
+
+        // Есть ли у игрока возможность выполнить слияние хотя бы в одном направлении:
+        foreach (var tile in tiles)
+        {
+            TileCell up = grid.GetAdjacentCell(tile.cell, Vector2Int.up);
+            TileCell down = grid.GetAdjacentCell(tile.cell, Vector2Int.down);
+            TileCell left = grid.GetAdjacentCell(tile.cell, Vector2Int.left);
+            TileCell right = grid.GetAdjacentCell(tile.cell, Vector2Int.right);
+
+            if (up != null && CanMerge(tile, up.tile))
+            {
+                return false;
+            }
+
+            if (down != null && CanMerge(tile, down.tile))
+            {
+                return false;
+            }
+
+            if (left != null && CanMerge(tile, left.tile))
+            {
+                return false;
+            }
+
+            if (right != null && CanMerge(tile, right.tile))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
